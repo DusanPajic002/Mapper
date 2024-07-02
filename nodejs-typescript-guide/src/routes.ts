efdd445570b05c9e66397abe163bae42826382e8
DusanPajic002 <dusanpajic2002@gmail.com>
2024-07-02 17:45:21 +0200

    fixed errors in printing res, and isValidURL is fixed

--- nodejs-typescript-guide/src/routes.ts
+++ nodejs-typescript-guide/src/routes.ts
@@ -13,37 +13,34 @@ router.get('/files', async (req: Request, res: Response) => {
     try {
         const tree = new Tree("root");
         const response = await axios.get("https://rest-test-eight.vercel.app/api/test");
-        const data = response.data.items; 
+        const data = response.data.items;
         for (let i = 0; i < data.length; i++) {
+
             let line = data[i].fileUrl;
-            if (!isValidURL(line)){ 
+            if (!isValidURL(line))
                 continue;
-            }
+        
             line = line.slice(("https://").length);
             line = line.split("/");
-            let node: TreeNode = tree.findOrAddNode(line[0], tree.getRoot());
-            for (let j = 1; j < line.length - 1; j++) {
+            let node: TreeNode = tree.findOrAddNode(line[0].split(":")[0], tree.getRoot());
+            for (let j = 1; j < line.length - 1; j++)
                 node = tree.findOrAddNode(line[j], node);
-            }
+            
         }
 
-        let print = tree.returnTree();
+        const formatedTree = tree.formatedTree();
         res.setHeader('Content-Type', 'application/json');
-        res.send(print);
+        res.send(formatedTree);
     } catch (error) {
         res.status(500).send('Error fetching files');
     }
 });
 
 function isValidURL(line: string) {
-    try {
-      new URL(line);
-      return true;
-    } catch (e) {
-        console.log(line)
-      return false;
-    }
-  }
+    const regex = /^[a-zA-Z0-9,\\._\-/:%]+$/;
+    console.log(regex.test(line));
+    return regex.test(line);
+}
 
 class Tree {
     private root: TreeNode;
@@ -53,11 +50,10 @@ class Tree {
     }
 
     findOrAddNode(name: string, node: TreeNode): TreeNode {
-        for (let child of node.children) {
-            if (child.name === name) {
+        for (let child of node.children)
+            if (child.name === name)
                 return child;
-            }
-        }
+        
         return this.addNode(node, name);
     }
 
@@ -74,30 +70,31 @@ class Tree {
         return this.root;
     }
 
-    returnTree(): string {
+    formatedTree(): string {
         let res = ""
         let n = this.root.children.length;
-        for (let i = 0; i < n; i++) {
-            res += this.createTree(this.root.children[i])
-        }
+        for (let i = 0; i < n; i++)
+            res += this.create(this.root.children[i])
         return res;
     }
 
-    private createTree(node: TreeNode, childNumber: number = 0): string {
-        let res = '';
+    private create(node: TreeNode): string {
+        let res = "";
 
         if (node.children.length > 0) {
-            res += `{`;
-            res += `\"${node.name}\": [`;
-            for (let i = 0; i < node.children.length; i++)
-                res += this.createTree(node.children[i], node.children.length - i);
-            res += `]`;
-            res += (childNumber == 1) ? "}," : "}";
-        } else
-            res += (childNumber == 1) ? `\"${node.name}\"` : `\"${node.name}\",`;
-
+            res += `{ "${node.name}": [`;
+            for (let i = 0; i < node.children.length; i++) {
+                res += this.create(node.children[i]);
+                if (i < node.children.length - 1)
+                    res += ", ";
+            }
+            res += `] }`;
+        } else  res += `"${node.name}"`;
+        
         return res;
     }
+
+
 }
- 
+
 export default router;
